380f2575ff47be8e57a2daad0e4acd6d0cd307dc
zzazz <zzazz@gmail.com>
2021-12-06 14:54:53 +0900

    fourth_commit

--- src/main/java/com/atzzazz/dao/UserDao.java
+++ src/main/java/com/atzzazz/dao/UserDao.java
@@ -1,10 +1,13 @@
 package com.atzzazz.dao;
 
+import org.springframework.stereotype.Repository;
+
 /**
- * @Description：$
+ * @Description：userDao
  * @Author： zzazz
  * @Date： 2021/12/6 - 11:50
  */
+@Repository
 public class UserDao {
     public void printUserDao(){
         System.out.println("UserDao方法被调用.....");
--- src/main/java/com/atzzazz/service/UserService.java
+++ src/main/java/com/atzzazz/service/UserService.java
@@ -1,23 +1,30 @@
 package com.atzzazz.service;
 
 import com.atzzazz.dao.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
 /**
- * @Description：$
+ * @Description：
  * @Author： zzazz
  * @Date： 2021/12/6 - 11:49
  */
+@Service
 public class UserService {
 
+    @Autowired
+    @Qualifier("userDao")
     private UserDao userDao;
 
-    public void setUserDao(UserDao userDao) {
-        this.userDao = userDao;
-    }
+    @Value("爱主主")
+    private String name;
 
     public void printUserService() {
         System.out.println("UserService方法被调用.....");
         userDao.printUserDao();
+        System.out.println(this.userDao);
     }
 
 
