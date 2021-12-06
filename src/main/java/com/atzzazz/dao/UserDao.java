7c6b4a59a3ee4cf2050979d52fdf688e7e81feef
zzazz <zzazz@gmail.com>
2021-12-06 12:06:23 +0900

    first_commit

---
+++ src/main/java/com/atzzazz/dao/UserDao.java
@@ -0,0 +1,12 @@
+package com.atzzazz.dao;
+
+/**
+ * @Description：$
+ * @Author： zzazz
+ * @Date： 2021/12/6 - 11:50
+ */
+public class UserDao {
+    public void printUserDao(){
+        System.out.println("UserDao.....");
+    }
+}
---
+++ src/main/java/com/atzzazz/service/UserService.java
@@ -0,0 +1,13 @@
+package com.atzzazz.service;
+
+/**
+ * @Description：$
+ * @Author： zzazz
+ * @Date： 2021/12/6 - 11:49
+ */
+public class UserService {
+
+    public void printUserService(){
+        System.out.println("UserService.....");
+    }
+}
